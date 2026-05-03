906e151e57d42d24dbf47a0558bdca2ccbab6893
Ast07221 <as.thakahov@yandex.ru>
2026-05-03 13:53:56 +0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,27 @@
+const countries = [
+{c:"USA",p:"+1"},
+{c:"UK",p:"+44"},
+{c:"Germany",p:"+49"},
+{c:"France",p:"+33"},
+{c:"Netherlands",p:"+31"},
+{c:"Spain",p:"+34"},
+{c:"Italy",p:"+39"},
+{c:"Japan",p:"+81"},
+{c:"China",p:"+86"},
+{c:"India",p:"+91"},
+{c:"UAE",p:"+971"},
+{c:"Canada",p:"+1"},
+{c:"Brazil",p:"+55"},
+{c:"Mexico",p:"+52"},
+{c:"Australia",p:"+61"},
+{c:"South Africa",p:"+27"},
+{c:"Sweden",p:"+46"},
+{c:"Norway",p:"+47"},
+{c:"Poland",p:"+48"},
+{c:"Turkey",p:"+90"}
+];
+
+const firstNames=["Alex","John","Max","David","Leo","Chris","Daniel","Ivan","Omar","Liam","Noah","Mason","Lucas","Ethan","Hiro","Carlos"];
+const lastNames=["Smith","Brown","Taylor","Johnson","Lee","Martin","Garcia","Khan","Ivanov","Schmidt","Silva","Costa","Ali"];
+
+const emails=["gmail.com","outlook.com","yahoo.com","proton.me"];
